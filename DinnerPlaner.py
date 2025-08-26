3f99c0b80164df293f29fdd55135eef81291e605
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-08-26 19:34:40 +0300

    Update DinnerPlaner.py

--- DinnerPlaner.py
+++ DinnerPlaner.py
@@ -9,3 +9,15 @@ connection = psycopg2.connect(database = DATABASE,
     port = PORT)
 
 cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS recipes (
+               id_recipe INTEGER PRIMARY KEY,
+               meal_id TEXT UNIQUE,
+               name TEXT,
+               category TEXT,
+               area TEXT,
+               instructions TEXT
+               )
+
+''')
