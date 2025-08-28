afa11c613bdc0a778729d4c98d2a7841be77cdfe
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-08-28 17:58:55 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,15 +1,15 @@
-# DATABASE = 'neondb'
-# USER = 'neondb_owner'
-# PASSWORD = 'npg_Gi80OvuZSLAj'
-# HOST = 'ep-fancy-mud-ab3ddiwy.eu-west-2.aws.neon.tech'
-# sslmode="require",
-# options="-c channel_binding=require"
-# PORT = 5432
+DATABASE = 'neondb'
+USER = 'neondb_owner'
+PASSWORD = 'npg_Gi80OvuZSLAj'
+HOST = 'ep-fancy-mud-ab3ddiwy.eu-west-2.aws.neon.tech'
+sslmode="require",
+options="-c channel_binding=require"
+PORT = 5432
 
-DATABASE = 'Recipes'
-USER = 'postgres'
-PASSWORD = 'test'
-HOST = 'localhost'
-# sslmode="require",
-# options="-c channel_binding=require"
-PORT = '5432'
+# DATABASE = 'Recipes'
+# USER = 'postgres'
+# PASSWORD = 'test'
+# HOST = 'localhost'
+# # sslmode="require",
+# # options="-c channel_binding=require"
+# PORT = '5432'
