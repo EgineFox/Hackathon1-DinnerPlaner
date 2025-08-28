f4b97bc3a253e4444b81f14d6812bb81ac0eca2b
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2025-08-28 14:04:59 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,5 +1,7 @@
-DATABASE = 'Recipes'
-USER = 'postgres'
-PASSWORD = 'test'
-HOST = 'localhost'
-PORT = '5432'
+DATABASE = 'neondb'
+USER = 'neondb_owner'
+PASSWORD = 'npg_Gi80OvuZSLAj'
+HOST = 'ep-fancy-mud-ab3ddiwy.eu-west-2.aws.neon.tech'
+sslmode="require",
+options="-c channel_binding=require"
+PORT = 5432
